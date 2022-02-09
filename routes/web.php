732162052557697d9b300000f5f106cb9e18db3d
Alvi Hasan <livealvi@gmail.com>
2022-02-09 20:27:08 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//Controllers
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\ContactControllers;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//nav-bar
+Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/service',[ProductController::class, 'service'])->name('service');
+Route::get('/product',[ProductController::class, 'product'])->name('product');
+Route::get('/contact',[ProductController::class, 'contact'])->name('contact');
+Route::get('/teams',[ProductController::class, 'ourTeams'])->name('teams');
+Route::get('/about',[ProductController::class, 'about'])->name('about');
+
+//login
+Route::get('/login',[LoginController::class, 'login'])->name('login');
+Route::post('/hone',[LoginController::class, 'loginValidator'])->name('loginValidator');
+
+//registration
+Route::get('/registration',[RegistrationController::class, 'registration'])->name('registration');
+Route::post('/registration', [RegistrationController::class, 'userCreate'])->name('userCreate');
+
+//users
+Route::get('/users',[RegistrationController::class, 'users'])->name('users');
+
+//Contact
+Route::post('/contact', [ContactControllers::class, 'contactUs'])->name('contactUs');
