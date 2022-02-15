9cd6a9fa33f233a7b1ee0fa4c46939a80e6c37d1
Alvi Hasan <livealvi@gmail.com>
2022-02-16 02:24:15 +0600

    fix: navbar name :bug:

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -14,7 +14,7 @@
           <a class="nav-link" href="{{route('service')}}">Service</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{route('users')}}">Students</a>
+          <a class="nav-link" href="{{route('users')}}">Users</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{route('teachers.courses')}}">Teachers</a>
