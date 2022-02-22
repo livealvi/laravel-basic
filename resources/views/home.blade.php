d5ce45fc52613dc9bc14b8ed5adfb37ba0e316fa
Alvi Hasan <livealvi@gmail.com>
2022-02-23 02:01:36 +0600

    fix: home-page :bug:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="d-flex justify-content-center align-items-center vh-100">
-    <h1>Home -
-        @if (session('user'))
-        {{session('user') ? session('user.0') : "null" }}</h1>
-
-    @else
-    {{session('teacher') ? session('teacher.0') : "null" }}</h1>
-    @endif
-    {{
-    dd(
-    Session::all()
-    )
-    }}
+    <h1>Home</h1>
 </div>
 
 @endsection
